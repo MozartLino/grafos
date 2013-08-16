dffef528304c744ffc679b92de5126809b864a84
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-08-16 01:39:44 -0300

    inicializando grafo

--- src/busca/BuscaEmProfundidade.c
+++ src/busca/BuscaEmProfundidade.c
@@ -2,27 +2,56 @@
 #include <stdlib.h>
 
 typedef struct NO {
-	int aux;
+	int valor;
 	struct NO *prox;
 } NO;
 
-NO* createGrafo() {
-	NO *array = (NO *) calloc (5, sizeof (NO));
-	int i;
+NO *createNewNO(int valor) {
+	NO *novo = (NO *) malloc(sizeof(NO));
+	novo->valor = valor;
+	novo->prox = NULL;
 
-	for (i = 1; i <= 5; ++i) {
-			array[i].aux = i;
-			array[i].prox = NULL;
-	}
+	return novo;
+}
+
+NO *createGrafo() {
+	NO *array = (NO *) calloc(5, sizeof(NO));
+
+	NO *p = createNewNO(2);
+	p->prox = createNewNO(3);
+	array[1].valor = 1;
+	array[1].prox = p;
+
+	NO *p2 = createNewNO(1);
+	p2->prox = createNewNO(5);
+	array[2].valor = 2;
+	array[2].prox = p2;
+
+	NO *p3 = createNewNO(4);
+	p3->prox = createNewNO(2);
+	array[3].valor = 3;
+	array[3].prox = p3;
+
+	NO *p4 = createNewNO(2);
+	p4->prox = createNewNO(3);
+	array[4].valor = 4;
+	array[4].prox = p4;
 
 	return array;
 }
 
-void buscaEmProfundidade(NO *g){
+void buscaEmProfundidade(NO *g) {
 	int i;
 
-	for (i = 1; i <= 5; ++i) {
-			printf("%d", g[i].aux);
+	for (i = 1; i <= 4; ++i) {
+		printf("%d", g[i].valor);
+		printf("\n");
+		NO *p = g[i].prox;
+		while (p) {
+			printf("%d", p->valor);
+			p = p->prox;
+		}
+		printf("\n");
 	}
 
 }
