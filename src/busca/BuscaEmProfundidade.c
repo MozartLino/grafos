20a5b82c5bb6f0886d42dd4b0f2315ae349b087f
Ricardo Mozart Lino <ricardo.mozart@gmail.com>
2013-08-20 00:33:12 -0300

    melhorando logica de criacao de matriz

--- src/busca/BuscaEmProfundidade.c
+++ src/busca/BuscaEmProfundidade.c
@@ -16,26 +16,41 @@ NO *createNewNO(int valor) {
 
 NO *createGrafo() {
 	NO *array = (NO *) malloc(5 * sizeof(NO));
+	NO *aux;
+	int matriz[5][5] = { { 2, 3 }, { 1, 5 }, { 1 }, { 2, 3 }, { 4 } };
 
-	NO *p = createNewNO(2);
-	p->prox = createNewNO(3);
-	array[1].valor = 1;
-	array[1].prox = p;
+	int i, j;
+	for (i = 0; i < 5; ++i) {
+		array[i].valor = i;
+		array[i].prox = NULL;
 
-	NO *p2 = createNewNO(1);
-	p2->prox = createNewNO(5);
-	array[2].valor = 2;
-	array[2].prox = p2;
+		aux = array[i];
+		for (j = 0; j < 5; ++j) {
+			NO *p = createNewNO(matriz[i][j]);
+			aux->prox = p;
+			aux = p;
+		}
+	}
 
-	NO *p3 = createNewNO(4);
-	p3->prox = createNewNO(2);
-	array[3].valor = 3;
-	array[3].prox = p3;
-
-	NO *p4 = createNewNO(2);
-	p4->prox = createNewNO(3);
-	array[4].valor = 4;
-	array[4].prox = p4;
+//	NO *p = createNewNO(2);
+//	p->prox = createNewNO(3);
+//	array[1].valor = 1;
+//	array[1].prox = p;
+//
+//	NO *p2 = createNewNO(1);
+//	p2->prox = createNewNO(5);
+//	array[2].valor = 2;
+//	array[2].prox = p2;
+//
+//	NO *p3 = createNewNO(4);
+//	p3->prox = createNewNO(2);
+//	array[3].valor = 3;
+//	array[3].prox = p3;
+//
+//	NO *p4 = createNewNO(2);
+//	p4->prox = createNewNO(3);
+//	array[4].valor = 4;
+//	array[4].prox = p4;
 
 	return array;
 }
